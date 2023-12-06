db639fe38f66c3ef387d70476ed35cf8e8c9410e
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2023-12-06 22:46:45 +0800

    fixed decode function

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,18 +5,21 @@ let textAreaOne = "";
 let textAreaTwo = "";
 
 function encryptDecryptTextOne() {
+  let shift = count;
   const asciiA = "a".codePointAt(0);
   const asciiZ = "z".codePointAt(0);
   textAreaOne = document.getElementById("one").value.toLowerCase();
   let encMessage = "";
-
+  if (choice == 1) {
+    shift = shift * -1;
+  }
   for (let chr of textAreaOne) {
     let code = chr.codePointAt(0);
 
     if (code >= asciiA && code <= asciiZ) {
       code -= asciiA;
 
-      code = mod(code + count, 26);
+      code = mod(code + shift, 26);
       code += asciiA;
     }
     encMessage += String.fromCodePoint(code);
@@ -25,6 +28,7 @@ function encryptDecryptTextOne() {
 }
 
 function encryptDecryptTextTwo() {
+  let shift = count;
   const asciiA = "a".codePointAt(0);
   const asciiZ = "z".codePointAt(0);
   textAreaTwo = document.getElementById("two").value.toLowerCase();
@@ -38,7 +42,7 @@ function encryptDecryptTextTwo() {
     if (code >= asciiA && code <= asciiZ) {
       code -= asciiA;
 
-      code = mod(code + count * -1, 26);
+      code = mod(code + shift * -1, 26);
       code += asciiA;
     }
     encMessage += String.fromCodePoint(code);
@@ -63,8 +67,15 @@ function toShiftAlpha() {
   toShift = mod(toShift + count, 26);
   toShift += asciiA;
   console.log(toShift);
-  document.getElementById("to-shift-alpha").textContent =
-    String.fromCodePoint(toShift);
+  if (choice == 0) {
+    document.getElementById("to-shift-alpha-two").textContent =
+      String.fromCodePoint(toShift);
+    document.getElementById("to-shift-alpha-one").textContent = "a";
+  } else {
+    document.getElementById("to-shift-alpha-one").textContent =
+      String.fromCodePoint(toShift);
+    document.getElementById("to-shift-alpha-two").textContent = "a";
+  }
 }
 
 function shiftNumberPlus() {
@@ -89,12 +100,15 @@ function shiftNumberDirect() {
 }
 
 function choiceChange() {
+  let currentShiftValue1 = "";
+  let currentShiftValue2 = "";
+  let message1 = "";
+  let message2 = "";
   if (choice == 0) {
-    let message1 = "";
-    let message2 = "";
     document.getElementById("choice").innerHTML = "Decode";
     document.getElementById("first-text").innerHTML = "Cipher Text";
     document.getElementById("second-text").innerHTML = "Plain Text";
+    document.getElementById("shift-text").textContent = "Unshift";
     document.getElementById("choice").classList.remove("blue-color");
     document.getElementById("choice").classList.add("red-color");
     choice = 1;
@@ -103,12 +117,22 @@ function choiceChange() {
     console.log(message1, " ", message2);
     document.getElementById("one").value = message2;
     document.getElementById("two").value = message1;
+    currentShiftValue1 =
+      document.getElementById("to-shift-alpha-one").textContent;
+    currentShiftValue2 =
+      document.getElementById("to-shift-alpha-two").textContent;
+
+    document.getElementById("to-shift-alpha-two").innerHTML =
+      currentShiftValue1;
+    document.getElementById("to-shift-alpha-one").innerHTML =
+      currentShiftValue2;
   } else {
     let message1 = "";
     let message2 = "";
     document.getElementById("choice").innerHTML = "Encode";
     document.getElementById("first-text").innerHTML = "Plain Text";
     document.getElementById("second-text").innerHTML = "Cipher Text";
+    document.getElementById("shift-text").textContent = "Shift";
     document.getElementById("choice").classList.remove("red-color");
     document.getElementById("choice").classList.add("blue-color");
     choice = 0;
@@ -116,5 +140,15 @@ function choiceChange() {
     message2 = document.getElementById("two").value;
     document.getElementById("one").value = message2;
     document.getElementById("two").value = message1;
+
+    currentShiftValue1 =
+      document.getElementById("to-shift-alpha-two").textContent;
+    currentShiftValue2 =
+      document.getElementById("to-shift-alpha-one").textContent;
+
+    document.getElementById("to-shift-alpha-one").innerHTML =
+      currentShiftValue1;
+    document.getElementById("to-shift-alpha-two").innerHTML =
+      currentShiftValue2;
   }
 }
